aeec1b4d9acaea975a7fc9e1ab24b33f7882bbdb
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-02-24 11:29:48 +0330

    add

--- script.js
+++ script.js
@@ -9,19 +9,23 @@ const products = [
         name : "mascara",
         image: "img/gold.jpg",
         price : "20",
-        id : 0
+        id : 1
     },
     {
         name : "lip Stick",
         image:"img/lips.jpg",
         price : "13",
-        id : 0
+        id : 2
     },
     {
         name : "Eye Liner",
         image:"img/pen.jpg",
         price : "16",
-        id : 0
+        id : 3
     }
 ]
 
+const renderProducts = ()=> {
+   const productDiv = document.querySelector(".container");
+   productDiv.textContent = ""
+}
